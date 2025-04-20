4a91acd4ae7be711012f9a9a5a3098c74be39b95
Ali Ehab <aliehab@Alis-MacBook-Air.local>
2025-04-20 23:02:57 +0200

    modified the db config

--- grade-simulator-service/Backend/config/database.js
+++ grade-simulator-service/Backend/config/database.js
@@ -5,8 +5,8 @@ const sequelize = new Sequelize(
   process.env.DB_USER || 'root',
   process.env.DB_PASSWORD || '',
   {
-    host: process.env.DB_HOST || 'mysql', // Updated to match the Kubernetes service name
-    port: process.env.DB_PORT || 3306,
+    host: process.env.DB_HOST || 'localhost', // Updated to match the Kubernetes service name
+    port: process.env.DB_PORT || 3308,
     dialect: 'mysql',
     logging: false, // Set to console.log for debugging if needed
     retry: {
