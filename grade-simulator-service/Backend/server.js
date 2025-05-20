95cd9c8b4437672a44e436e5a3fc1e65562a8a8b
Ali Ehab <aliehab@Alis-MacBook-Air.local>
2025-05-21 01:21:42 +0300

    updated cors policy

--- grade-simulator-service/Backend/server.js
+++ grade-simulator-service/Backend/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URLq
+  origin: '*', // Allow all origins
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
